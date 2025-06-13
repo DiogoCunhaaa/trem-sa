cb5037382179e78940acc4ff14e325bb14fc37a5
JOÃO VITOR DA SILVA VIEIRA <joao-vitor_vieira@estudante.sesisenai.org.br>
2025-06-13 08:16:07 -0300

    Botão do reboque respondendo ao clicar nele

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,9 +1,9 @@
 import styles from './styles/CustomButton.module.css'
 
-function CustomButton({ whatFor, backgroundColor, buttonTextColor }) {
+function CustomButton({ whatFor, backgroundColor, buttonTextColor,onClick }) {
     return(
         <div className='m-2'> 
-            <button className={`${styles.customButton} fw-bold`} style={{ backgroundColor: backgroundColor, color: buttonTextColor}}>{whatFor}</button>
+            <button className={`${styles.customButton} fw-bold`} style={{ backgroundColor: backgroundColor, color: buttonTextColor}} onClick={onClick}>{whatFor}</button>
         </div>
     )
 };
