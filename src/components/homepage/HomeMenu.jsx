cfb2ed1a2f7c8901c10d6490224c7312bb4d8c79
JoaoVieiraa07 <joao-vitor_vieira@estudante.sesisenai.org.br>
2025-10-10 19:14:12 -0300

    Mudança de foto na home page

--- src/components/homepage/HomeMenu.jsx
+++ src/components/homepage/HomeMenu.jsx
@@ -5,7 +5,7 @@ function HomeMenu() {
     return(
         <>
         <HomeButton 
-        imageUrl="https://i.pinimg.com/736x/14/d7/df/14d7df61dc1701521d25761a48dab232.jpg"
+        imageUrl="https://images.unsplash.com/photo-1630824869986-a5b2f513b1f4?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1632"
         to="/dashboard"
         label="DASHBOARD"
       />
