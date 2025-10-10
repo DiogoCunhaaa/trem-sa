fff46fb9c83ba06a2398936f104d409e509ef0f7
JoaoVieiraa07 <joao-vitor_vieira@estudante.sesisenai.org.br>
2025-10-10 19:55:45 -0300

    Criação das caixas de texto para mudança de perfil

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,20 +1,48 @@
-function Perfil(){
-    return(
+function Perfil() {
+  return (
     <div className="container">
-        <div>
-            <img
-            src='/favicon.ico'
-            className='mx-auto mt-5 d-block shadow-sm rounded-circle'
-            alt='icone'
-            width={'200px'}
-            height={'200px'}
-            />
+      <div>
+        <img
+          src="/favicon.ico"
+          className="mx-auto d-block shadow-sm rounded-circle"
+          alt="icone"
+          width="200px"
+          height="200px"
+        />
       </div>
-      <div className='text-center my-3'>
-        <h4>PERFIL</h4>
+
+      <div className="text-center my-3">
+        <h4>SEU PERFIL</h4>
+      </div>
+
+      <div className="center shadow p-4 mb-3 bg-body rounded d-flex justify-content-between align-items-center">
+        <span>Seu nome de usuário é "admin"</span>
+        <input
+          type="text"
+          placeholder="Alterar nome usuário"
+          className="form-control w-50"
+        />
+      </div>
+
+      <div className="center shadow p-4 mb-3 bg-body rounded d-flex justify-content-between align-items-center">
+        <span>Seu e-mail é "**m**@gmail.com"</span>
+        <input
+          type="text"
+          placeholder="Alterar email"
+          className="form-control w-50"
+        />
+      </div>
+
+       <div className="center shadow p-4 mb-5 bg-body rounded d-flex justify-content-between align-items-center">
+        <span>Sua senha é "*****"</span>
+        <input
+          type="text"
+          placeholder="Alterar senha"
+          className="form-control w-50"
+        />
       </div>
     </div>
-    )
+  );
 }
 
 export default Perfil;
